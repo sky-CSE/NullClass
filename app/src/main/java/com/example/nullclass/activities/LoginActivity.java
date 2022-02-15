f1802a0e7fb34653a18d2022b4736fcb04681b7f
Sumeet Kumar Yadav <80821238+sky-CSE@users.noreply.github.com>
2022-02-15 21:04:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/nullclass/activities/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.nullclass.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+import android.os.Bundle;
+
+import com.example.nullclass.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
